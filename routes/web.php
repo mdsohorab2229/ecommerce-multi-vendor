432e725a98ebfdd302d0359d1492f94457217145
DESKTOP-D3ME4O3\Md Sohorab Hossain <mdsohorab2229@gmail.com>
2023-09-30 00:31:15 -0700

    create admin meddileware and check

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,9 @@ require __DIR__.'/auth.php';
 Route::prefix('/admin')->group(function () {
 
     Route::match(['get', 'post'],'login', [AdminController::class, 'login']);
-    Route::get('dashboard', [AdminController::class, 'dashboard']);
+    Route::group(['middleware'=>['admin']], function(){
+        Route::get('dashboard', [AdminController::class, 'dashboard']);
+    });
+    
 });
 
