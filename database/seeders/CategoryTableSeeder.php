fa829cfff29971f79115e8a154675ebe15a0bef8
mdsohorab2229 <mdsohorab2229@gmail.com>
2024-10-11 13:21:19 +0600

    create categories model migration controller and seeder file

---
+++ database/seeders/CategoryTableSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categoryRecords  = [
+            [
+                'parent_id' => 0,
+                'section_id' => 1,
+                'category_name' => 'Electronics',
+                'category_image' => 'electronics.jpg',
+                'category_discount' => 10.5,
+                'description' => 'Category for all kinds of electronics products',
+                'url' => 'electronics',
+                'meta_title' => 'Electronics Meta Title',
+                'meta_description' => 'Meta description for electronics category',
+                'meta_keywords' => 'electronics, gadgets, tech',
+                'status' => 1,
+            ],
+            [
+                'parent_id' => 0,
+                'section_id' => 2,
+                'category_name' => 'Fashion',
+                'category_image' => 'fashion.jpg',
+                'category_discount' => 15.0,
+                'description' => 'Category for fashion and clothing',
+                'url' => 'fashion',
+                'meta_title' => 'Fashion Meta Title',
+                'meta_description' => 'Meta description for fashion category',
+                'meta_keywords' => 'fashion, clothing, style',
+                'status' => 1,
+            ],
+            [
+                'parent_id' => 0,
+                'section_id' => 3,
+                'category_name' => 'Appliances',
+                'category_image' => 'appliances.jpg',
+                'category_discount' => 12.0,
+                'description' => 'Various appliances including kitchen and cleaning items',
+                'url' => 'appliances',
+                'meta_title' => 'Appliances Meta Title',
+                'meta_description' => 'Meta description for  appliances category',
+                'meta_keywords' => 'appliances, kitchen, cleaning',
+                'status' => 1,
+            ],
+            [
+                'parent_id' => 0,
+                'section_id' => 4,
+                'category_name' => 'Books',
+                'category_image' => 'books.jpg',
+                'category_discount' => 8.0,
+                'description' => 'Books across various genres and authors',
+                'url' => 'books',
+                'meta_title' => 'Books Meta Title',
+                'meta_description' => 'Meta description for books category',
+                'meta_keywords' => 'books, reading, literature',
+                'status' => 1,
+            ]
+        ];
+
+        Category::insert($categoryRecords);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call(VendorsBusinessDetailsTableSeeder::class);
         $this->call(VendorsBankDetailsTableSeeder::class);
         $this->call(SectionTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
     }
 }
